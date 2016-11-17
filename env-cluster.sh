6d2a18366d00e2fa65eacda45d5a8146bc6a45de
Ryan Griffin <rgriffin@massmutual.com>
2016-11-17 11:07:03 -0500

    Updated the bootstrap script to streamline the process of getting a new job running on the cluster. Update the env-cluster.sh file to be a little cleaner.

--- env-cluster.sh
+++ env-cluster.sh
@@ -1,23 +1,31 @@
+# VERTICA SETTINGS
 export VERTICA_CONNECTION_ID="vertica_dev"
 export VERTICA_HOST="vertica-dev"
 export VERTICA_USER="dbadmin"
-export PY4JDBC_JAR="/hdfs/deploy/jdbc/py4jdbc-assembly-latest.jar"
-#export SQUARK_PYTHON=$(pwd)/virt/bin/python
 export VERTICA_DATABASE="advana"
-export SPARK_HOME=/hadoop/spark/1.6
-#export PYSPARK_PYTHON=$(pwd)/virt/bin/python
-export PYSPARK_PYTHON=$SPARK_HOME/bin/python
-export SPARK_DRIVER_MEMORY="2G"
-export SPARK_EXECUTOR_MEMORY="2G"
+export VERTICA_VSQL="/usr/local/bin/vsql"
+
+# HDFS SETTINGS
 export HDFS_HOST="devlx187"
 export HDFS_PORT="50070"
 export HDFS_USER="jenkins"
-export JARS="${PY4JDBC_JAR}" # comma separated
 export HADOOP_CONF_DIR=/etc/hadoop/conf
+
+# JARS
+export PY4JDBC_JAR="/hdfs/deploy/jdbc/py4jdbc-assembly-latest.jar"
+export JARS="${PY4JDBC_JAR}" # comma separated
+
+# SQUARK SETTINGS
 export SQUARK_PASSWORD_FILE=$(pwd)/.squark_password
 export SQUARK_CONFIG_DIR=$(pwd)/config
 export PYTHON_VENV=$(pwd)/virt
 export WAREHOUSE_DIR="/_wh_dev/"
-#export SQUARK_PYTHON=$SPARK_HOME/python
+
+# SPARK SETTINGS
+export SPARK_HOME=/hadoop/spark/1.6
+export PYSPARK_PYTHON=$PYTHON_VENV/bin/python
+export SPARK_DRIVER_MEMORY="2G"
+export SPARK_EXECUTOR_MEMORY="2G"
 export CLASSPATH=$CLASSPATH:$PY4JDBC_JAR
 export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python:$(pwd)
+
