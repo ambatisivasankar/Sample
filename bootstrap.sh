58473466e5fa66bf9f12458def3e5028b8bb4689
Owen Galvin <ogalvin@massmutual.com>
2017-02-24 12:51:22 -0500

    test squark requirements.txt w/--no-cache-dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 virtualenv -p /usr/bin/python3 virt
 /usr/local/src/squark-advana/virt/bin/pip3 install -r squark-classic/requirements.txt
 cd squark
-/usr/local/src/squark-advana/virt/bin/pip3 install -r requirements.txt
+/usr/local/src/squark-advana/virt/bin/pip3 install -r --no-cache-dir requirements.txt
 /usr/local/src/squark-advana/virt/bin/python3 setup.py develop
 cd ../data_catalog-statscli
 /usr/local/src/squark-advana/virt/bin/pip3 install -r requirements.txt
