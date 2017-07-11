4748d6a5e0a929333c013a10a6ae210b0f461581
Owen Galvin <ogalvin@massmutual.com>
2017-07-11 15:34:49 -0400

    add aws-env-cluster.sh, with some aws specific settings

---
+++ aws-env-cluster.sh
@@ -0,0 +1,31 @@
+# VERTICA SETTINGS
+export VERTICA_CONNECTION_ID="vertica_aws_nprd"
+export VERTICA_HOST="vertica.dsawsnprd.massmutual.com"
+export VERTICA_USER="dbadmin"
+export VERTICA_DATABASE="advana"
+export VERTICA_VSQL="/usr/local/bin/vsql"
+
+# HDFS SETTINGS
+export HDFS_HOST="ip-address-of-namenode"
+export HDFS_PORT="50070"
+export HDFS_USER="ec2-user"
+export HADOOP_CONF_DIR=/etc/hadoop/conf
+
+# JARS
+export PY4JDBC_JAR="/hadoop/deploy/jdbc/py4jdbc-assembly-0.1.6.8_210.jar"
+export JARS="py4jdbc_latest.jar" # comma separated
+
+# SQUARK SETTINGS
+export SQUARK_PASSWORD_FILE=$(pwd)/.squark_password
+export SQUARK_CONFIG_DIR=$(pwd)/config
+export PYTHON_VENV=$(pwd)/virt
+export WAREHOUSE_DIR="/_wh_dev/"
+
+# SPARK SETTINGS
+export SPARK_HOME=/hadoop/spark/1.6
+export PYSPARK_PYTHON=$PYTHON_VENV/bin/python
+export SPARK_DRIVER_MEMORY="2G"
+export SPARK_EXECUTOR_MEMORY="2G"
+export CLASSPATH=$CLASSPATH:$PY4JDBC_JAR
+export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python:$(pwd)
+
