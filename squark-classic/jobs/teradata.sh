eb7a52a45d24529246241972694f1b99a356293e
Owen Galvin <ogalvin@massmutual.com>
2017-07-20 14:57:42 -0400

    add partitioning for teradata (copy from teradata_prd)
    data source for teradata_prd=teradata
    tables being partitioned in _prd are present in base teradata

--- squark-classic/jobs/teradata.sh
+++ squark-classic/jobs/teradata.sh
@@ -4,18 +4,43 @@ export SQL_TEMPLATE='%s'
 export INCLUDE_VIEWS=1
 export INCLUDE_TABLES='AGMT_CMN_VW,CUST_AGMT_CMN_VW,CUST_DEMOGRAPHICS_VW,PDCR_AGMT_CMN_VW,PDCR_DEMOGRAPHICS_VW,AGMT_UWRT_CMN_VW,PDCR_ALT_ID_CMN_VW,CUST_PREFERENCE_VW,SLLNG_AGMT_CMN_VW,AGMT_VAL_CMN_VW,AGMT_CVG_CMN_VW'
 export CONNECTION_ID=teradata
-#export STATS_CONFIG="
-#{
-#    'profiles': {
-#        'numeric': ['max','min','mean','countDistinct','count_null'],
-#        'string': ['max', 'min','countDistinct','count_null'],
-#        'datetype': ['max', 'min','countDistinct','count_null']
-#    },
-#    'field_types': {
-#        'NumericType': 'numeric',
-#        'StringType': 'string',
-#        'DateType': 'datetype',
-#        'TimestampType': 'datetype'
-#    }
-#}
-#"
+
+# need to occasionally monitor CUST_DEMOGRAPHICS_VW for further growth in PRTY_ID values
+export JSON_INFO="
+{
+    'PARTITION_INFO':{
+        'tables': {
+            'AGMT_CMN_VW': {
+              'partitionColumn': 'AGREEMENT_ID',
+              'lowerBound': 1,
+              'upperBound': 34000000,
+              'numPartitions': 50
+            },
+            'AGMT_VAL_CMN_VW': {
+              'partitionColumn': 'AGREEMENT_ID',
+              'lowerBound': 1,
+              'upperBound': 34000000,
+              'numPartitions': 50
+            },
+            'PDCR_AGMT_CMN_VW': {
+              'partitionColumn': 'AGREEMENT_ID',
+              'lowerBound': 1,
+              'upperBound': 34000000,
+              'numPartitions': 50
+            },
+            'CUST_AGMT_CMN_VW': {
+              'partitionColumn': 'AGREEMENT_ID',
+              'lowerBound': 1,
+              'upperBound': 34000000,
+              'numPartitions': 50
+            },
+            'CUST_DEMOGRAPHICS_VW': {
+              'partitionColumn': 'PRTY_ID',
+              'lowerBound': 1,
+              'upperBound': 20000000,
+              'numPartitions': 50
+            }
+        }
+   }
+}
+"
