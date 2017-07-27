afced291ff2d3c77f62323c93ee2dbffee64821b
Owen Galvin <ogalvin@massmutual.com>
2017-07-27 09:56:03 -0400

    add Sybase to the source dbs being logged for row counts

--- squark-classic/all_tables.py
+++ squark-classic/all_tables.py
@@ -142,7 +142,8 @@ def convert_array_to_string(df):
 
 def log_source_row_count(sqlctx, table_name, properties, db_product_name):
     count = None
-    handled_db_prefixes = ['teradata','postgres']
+    # 'ase' = Sybase
+    handled_db_prefixes = ['teradata','postgres','ase']
     if any(db_product_name.lower().startswith(db) for db in handled_db_prefixes):
         sql_query = '(SELECT COUNT(*) as cnt FROM "{}") as query'.format(table_name)
         print('--- Executing source row count query: {}'.format(sql_query, flush=True))
