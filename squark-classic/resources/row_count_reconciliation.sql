e7c8a4f616fe5dcf9686bada6bdbccf2d6a4caa8
Owen Galvin <ogalvin@massmutual.com>
2017-08-07 12:22:04 -0400

    update the row reconciliation sql with before/after counts logic

--- squark-classic/resources/row_count_reconciliation.sql
+++ squark-classic/resources/row_count_reconciliation.sql
@@ -18,6 +18,7 @@ WITH cteAllTables AS
 		GROUP BY schema_name, projection_name
 	) A
 )
+-- select * from cteDeleted ORDER BY schema_name, projection_name
 ,cteUnadjustedRowCounts AS
 (
 	SELECT A.schema_name, A.anchor_table_name, A.projection_name, SUM(A.row_count) as total_row_count
@@ -26,6 +27,7 @@ WITH cteAllTables AS
 		projection_schema AS schema_name,
 		anchor_table_name,
 		projection_name,
+		
 		row_count,
 	ROW_NUMBER() OVER (PARTITION BY node_name, projection_schema, anchor_table_name ORDER BY projection_name) AS row_num
 	FROM projection_storage ps
@@ -33,6 +35,8 @@ WITH cteAllTables AS
 	WHERE A.row_num = 1
 	GROUP BY A.schema_name, A.anchor_table_name, A.projection_name
 )
+--SELECT * FROM cteUnadjustedRowCounts
+--WHERE schema_name = 'esp' ORDER BY schema_name, anchor_table_name
 ,cteFinalRowCounts AS
 (
 	SELECT u.schema_name, u.projection_name, u.anchor_table_name,
@@ -42,36 +46,59 @@ WITH cteAllTables AS
 	FROM cteUnadjustedRowCounts u
 	LEFT JOIN cteDeleted d ON d.schema_name = u.schema_name AND d.projection_name = u.projection_name
 )
-,cteCombined AS
+--SELECT * FROM cteFinalRowCounts ORDER BY schema_name, anchor_table_name
+,cteCombined AS 
 (
 	SELECT a.schema_name, a.table_name, f.final_row_count as row_count, a.schema_create_time, COUNT(*) OVER (PARTITION BY a.schema_name) AS TableCnt,
 		SUM(f.has_data) OVER (PARTITION BY a.schema_name) as PopdTblsCount
 	FROM cteAllTables a
 	LEFT JOIN cteFinalRowCounts f ON f.schema_name = a.schema_name AND f.anchor_table_name = a.table_name
 )
+--SELECT * FROM cteCombined ORDER BY schema_name, table_name
 ,cteSourceCounts AS
 (
-	SELECT A.project_id, A.source_schema, A.table_name, A.row_count, A.query_date, A.build_number, A.job_name, A.seconds_query_duration
+	SELECT A.project_id, A.source_schema, A.table_name, A.row_count, A.query_date, A.build_number, A.job_name, A.seconds_query_duration, A.is_after
 	FROM (
-		-- below is more flexible vs. passing in build number as a variable..
 		SELECT src.*, MAX(build_number) OVER (PARTITION BY src.project_id) as lastProjectBuild
 		FROM admin.squark_source_row_counts src
 		WHERE 1=1
 			AND src.project_id IN (SELECT DISTINCT a.schema_name FROM cteAllTables a)
 			AND src.query_date > SYSDATE() - 10
 	) A
-	WHERE A.build_number = lastProjectBuild
+	WHERE A.build_number = lastProjectBuild 
 )
-SELECT c.schema_name, c.table_name, c.row_count as VERT_row_count, src.row_count as SourceRowCount,
-	COALESCE(src.row_count, 0) - COALESCE(c.row_count, 0) as DIFF, CAST(CAST(src.seconds_query_duration as INT) as VARCHAR(10)) || ' secs' as querySecs,
-	c.schema_create_time, c.TableCnt,
-	CAST(c.PopdTblsCount AS VARCHAR(10))  || ' of ' || CAST(TableCnt AS VARCHAR(10)) as PopdTbls,	src.build_number, src.query_date as SourceQueryTime,
-	CASE WHEN src.row_count IS NULL THEN 'n.a.'
-		  WHEN COALESCE(src.row_count, 0) - COALESCE(c.row_count, 0) <> 0 THEN '>>>> WHOOPS row count <<<<'
-		  ELSE '.'
-	END as RowCountCheck
+--SELECT * FROM cteSourceCounts ORDER BY project_id, table_name   
+,cteAmalgSourceCounts AS
+(
+
+	SELECT scb.project_id, scb.source_schema, scb.table_name, scb.query_date as beforeQueryTime, scb.build_number as build, scb.job_name, 
+		scb.row_count as beforeRowCount, sca.row_count as afterRowCount, scb.seconds_query_duration as beforeDur, sca.seconds_query_duration as afterDur,
+		sca.row_count - scb.row_count as rawDiff, FLOOR(LN(scb.row_count)) as logSrcCount,
+		CASE WHEN sca.row_count IS NULL THEN scb.row_count
+			WHEN scb.row_count <= sca.row_count THEN scb.row_count
+			ELSE sca.row_count
+		END as lowSrcCount,
+		CASE WHEN sca.row_count IS NULL THEN scb.row_count
+			WHEN scb.row_count >= sca.row_count THEN scb.row_count
+			ELSE sca.row_count
+		END as highSrcCount
+	FROM cteSourceCounts scb
+	LEFT JOIN cteSourceCounts sca ON sca.project_id = scb.project_id AND sca.source_schema = scb.source_schema AND sca.table_name = scb.table_name AND sca.is_after = 't'
+	WHERE COALESCE(scb.is_after, 'f') = 'f' 
+)
+--SELECT * FROM cteAmalgSourceCounts
+SELECT c.schema_name, c.table_name, ROUND(c.schema_create_time, 'SS') AS schema_create_time, c.row_count as vertRowCount, src.lowSrcCount, src.highSrcCount, 
+	ROUND(src.beforeQueryTime, 'SS') as srcQueryTime, logSrcCount * 3 as tolDiff, src.lowSrcCount - COALESCE(c.row_count, 0) as rawDIFF, 
+	CAST(c.PopdTblsCount AS VARCHAR(10))  || ' of ' || CAST(TableCnt AS VARCHAR(10)) as vertPopdTbls, src.build,
+	CASE WHEN src.lowSrcCount IS NULL OR c.row_count BETWEEN src.lowSrcCount AND src.highSrcCount THEN '.'
+		-- arbitrary decision to declare any diff, vs. low count from source, <= logSrcCount * 3 to be "bad but not that bad"..
+		WHEN ABS(src.lowSrcCount - COALESCE(c.row_count, 0)) <= logSrcCount * 3 THEN '>>>> ooh, its close <<<<'
+		ELSE '>>>> WHOOPS row count <<<<' 
+	END AS RowCountCheck
+	-- debug stuff
+	--,logSrcCount, src.beforeDur, src.afterDur, src.beforeRowCount, src.afterRowCount
 FROM cteCombined c
-LEFT JOIN cteSourceCounts src ON src.project_id = c.schema_name AND src.table_name = c.table_name
+LEFT JOIN cteAmalgSourceCounts src ON src.project_id = c.schema_name AND src.table_name = c.table_name
 WHERE 1 = 1
-ORDER BY c.schema_name, UPPER(c.table_name)
+ORDER BY c.schema_name, c.table_name
 
