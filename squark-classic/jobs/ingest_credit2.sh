8d08e7d5919baad033783149a8e6603ce609b1e9
Owen Galvin <ogalvin@massmutual.com>
2018-01-16 16:11:31 -0500

    [INGEST-337] new ingest_credit2, 1 table from PRDWN10142.staging

---
+++ squark-classic/jobs/ingest_credit2.sh
@@ -0,0 +1,6 @@
+export PROJECT_ID=ingest_credit2
+export WAREHOUSE_DIR='/_wh/'
+export SQL_TEMPLATE='%s'
+export INCLUDE_TABLES="wrk_LGD_Download"
+export CONNECTION_ID=ingest_credit2
+export SQUARK_METADATA=1
