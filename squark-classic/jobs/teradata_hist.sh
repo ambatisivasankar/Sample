82a237eae698248b55e0f8b4c6469fd00f9c72b1
Owen Galvin <ogalvin@massmutual.com>
2018-04-19 09:02:30 -0400

    INGEST-325, add AGMT_HIST_VW to teradata_hist

--- squark-classic/jobs/teradata_hist.sh
+++ squark-classic/jobs/teradata_hist.sh
@@ -2,10 +2,12 @@ export PROJECT_ID=teradata_hist
 export WAREHOUSE_DIR='/_wh/'
 export SQL_TEMPLATE='%s'
 export INCLUDE_VIEWS=1
-export INCLUDE_TABLES='CUST_AGMT_HIST_VW,CUST_DEMOGRAPHICS_HIST_VW,AGMT_CVG_HIST_VW,SLLNG_AGMT_HIST_VW'
+export INCLUDE_TABLES='CUST_AGMT_HIST_VW,CUST_DEMOGRAPHICS_HIST_VW,AGMT_CVG_HIST_VW,SLLNG_AGMT_HIST_VW,AGMT_HIST_VW'
 export CONNECTION_ID=teradata
 
+export SKIP_SOURCE_ROW_COUNT=1
 export SPARK_MAX_EXECUTORS=15
+# 2018.04.18, partition results on AGMT_HIST_VW pretty bad, AGREEMENT_ID has bimodal-ish dist for this date range
 export JSON_INFO="
 {
     'PARTITION_INFO':{
@@ -26,6 +28,12 @@ export JSON_INFO="
               'partitionColumn': 'COALESCE(AGREEMENT_ID, 0)',
               'lowerBound': 5600000,
               'upperBound': 34000000,
+              'numPartitions': 50
+            },
+            'AGMT_HIST_VW': {
+              'partitionColumn': 'ISSUE_DT >= """2015-01-01""" AND AGREEMENT_ID',
+              'lowerBound': 5700000,
+              'upperBound': 40000000,
               'numPartitions': 50
             }
         }
