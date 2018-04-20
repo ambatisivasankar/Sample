c5c9562fc1224794539553d8aedf905953dc2f64
Owen Galvin <ogalvin@massmutual.com>
2018-04-20 10:11:16 -0400

    teradata_hist, change quoting patteron on PARTITION_INFO

--- squark-classic/jobs/teradata_hist.sh
+++ squark-classic/jobs/teradata_hist.sh
@@ -8,38 +8,39 @@ export CONNECTION_ID=teradata
 export SKIP_SOURCE_ROW_COUNT=1
 export SPARK_MAX_EXECUTORS=15
 # 2018.04.18, partition results on AGMT_HIST_VW pretty bad, AGREEMENT_ID has bimodal-ish dist for this date range
-export JSON_INFO="
+#   in order to use value in the subfilter, double-quote all innards of the JSON
+export JSON_INFO='
 {
-    'PARTITION_INFO':{
-        'tables': {
-            'CUST_AGMT_HIST_VW': {
-              'partitionColumn': 'AGREEMENT_ID',
-              'lowerBound': 1,
-              'upperBound': 34000000,
-              'numPartitions': 50
+    "PARTITION_INFO":{
+        "tables": {
+            "CUST_AGMT_HIST_VW": {
+              "partitionColumn": "AGREEMENT_ID",
+              "lowerBound": 1,
+              "upperBound": 34000000,
+              "numPartitions": 50
             },
-            'CUST_DEMOGRAPHICS_HIST_VW': {
-              'partitionColumn': 'PRTY_ID',
-              'lowerBound': 1,
-              'upperBound': 22000000,
-              'numPartitions': 50
+            "CUST_DEMOGRAPHICS_HIST_VW": {
+              "partitionColumn": "PRTY_ID",
+              "lowerBound": 1,
+              "upperBound": 22000000,
+              "numPartitions": 50
             },
-            'AGMT_CVG_HIST_VW': {
-              'partitionColumn': 'COALESCE(AGREEMENT_ID, 0)',
-              'lowerBound': 5600000,
-              'upperBound': 34000000,
-              'numPartitions': 50
+            "AGMT_CVG_HIST_VW": {
+              "partitionColumn": "COALESCE(AGREEMENT_ID, 0)",
+              "lowerBound": 5600000,
+              "upperBound": 34000000,
+              "numPartitions": 50
             },
-            'AGMT_HIST_VW': {
-              'partitionColumn': 'ISSUE_DT >= """2015-01-01""" AND AGREEMENT_ID',
-              'lowerBound': 5700000,
-              'upperBound': 40000000,
-              'numPartitions': 50
+            "AGMT_HIST_VW": {
+              "partitionColumn": "ISSUE_DT >= """2015-01-01""" AND AGREEMENT_ID",
+              "lowerBound": 5700000,
+              "upperBound": 40000000,
+              "numPartitions": 50
             }
         }
    }
 }
-"
+'
 #export STATS_CONFIG="
 #{
 #    'profiles': {
