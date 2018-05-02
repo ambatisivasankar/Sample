307ceea023694613deefcfdfdc24ee1664dcdca1
Bryant Moscon <bmoscon@gmail.com>
2018-05-02 11:20:28 -0400

    SEG-153 - new teradata tables to ingest (#189)
    * SEG-153 - new teradata tables to ingest
    
    * Update td_mkt_analytics.sh

---
+++ squark-classic/jobs/td_mkt_analytics.sh
@@ -0,0 +1,6 @@
+export PROJECT_ID=td_mkt_analytics
+export WAREHOUSE_DIR='/_wh/'
+export SQL_TEMPLATE='%s'
+export INCLUDE_VIEWS=1
+export INCLUDE_TABLES='Customer_Ethnicity'
+export CONNECTION_ID=td_mkt_analytics
