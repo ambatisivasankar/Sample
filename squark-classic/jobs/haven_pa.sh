6f05ac0524e840fd2e4903c59289b2421a865913
Owen Galvin <ogalvin@massmutual.com>
2018-05-11 11:05:43 -0400

    haven_pa, partition policy_doc table

--- squark-classic/jobs/haven_pa.sh
+++ squark-classic/jobs/haven_pa.sh
@@ -5,3 +5,18 @@ export CHECK_PRIVS=1
 export CONNECTION_ID=haven_pa
 export SQUARK_METADATA=1
 
+
+export JSON_INFO="
+{
+    'PARTITION_INFO':{
+            'policy_doc': {
+              'partitionColumn': 'DATE_PART('''MINUTE''', COALESCE(\\\"createdTime\\\", '''1970-01-01T00:00:00'''::timestamp))',
+              'lowerBound': 0,
+              'upperBound': 59,
+              'numPartitions': 60
+            }
+        }
+   }
+}
+"
+
