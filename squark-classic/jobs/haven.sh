63a04c590b5e93249623ee2fd2e3732ef61e9a2f
Owen Galvin <ogalvin@massmutual.com>
2018-05-15 09:54:15 -0400

    haven, partition analytics_container and raw_data tables

--- squark-classic/jobs/haven.sh
+++ squark-classic/jobs/haven.sh
@@ -12,6 +12,12 @@ export JSON_INFO="
 {
     'PARTITION_INFO':{
         'tables': {
+            'analytics_container': {
+              'partitionColumn': 'DATE_PART('''MINUTE''', COALESCE(\\\"createdTime\\\", '''1970-01-01T00:00:00'''::timestamp))',
+              'lowerBound': 0,
+              'upperBound': 59,
+              'numPartitions': 59
+            },
             'analytics_event': {
               'partitionColumn': 'DATE_PART('''MINUTE''', COALESCE(\\\"createdTime\\\", '''1970-01-01T00:00:00'''::timestamp))',
               'lowerBound': 0,
@@ -26,6 +32,12 @@ export JSON_INFO="
             },
             'policy_doc': {
               'partitionColumn': 'DATE_PART('''MINUTE''', COALESCE(\\\"createdTime\\\", '''1970-01-01T00:00:00'''::timestamp))',
+              'lowerBound': 0,
+              'upperBound': 59,
+              'numPartitions': 59
+            },
+            'raw_data': {
+              'partitionColumn': 'DATE_PART('''SECOND''', COALESCE(\\\"createdTime\\\", '''1970-01-01T00:00:00'''::timestamp))',
               'lowerBound': 0,
               'upperBound': 59,
               'numPartitions': 59
