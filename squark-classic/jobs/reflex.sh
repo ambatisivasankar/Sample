fd26d37d1327f0f3e30252c50c602a55d242dada
Owen Galvin <ogalvin@massmutual.com>
2018-05-29 12:48:39 -0400

    reflex, add new tables, partitioning for big tables, remove dupe

--- squark-classic/jobs/reflex.sh
+++ squark-classic/jobs/reflex.sh
@@ -2,13 +2,10 @@ export PROJECT_ID=reflex
 export WAREHOUSE_DIR='/_wh/'
 export SQL_TEMPLATE='%s'
 export EXCLUDE_TABLES="CMB_PARTC_TOT_MILESTONE_WORK,DB_RPT_REQUEST_TRACKING_WORK"
-#export INCLUDE_TABLES="PERSON,EXTERNAL_ADDRESS,partc_status_cde,PLAN_STATUS_CDE,PRODUCT_TYPE_CDE,MARITAL_STATUS_CDE,PARTC_STATUS_HIST,PARTC_STATUS_HIST,PARTC_SOURCE,PLAN_DCO,PARTICIPANT,PLAN_SPONSOR,PLAN_SUBSCRIPTION,BUSINESS_ENTITY,PARTC_SRC_ADI_EVENT,PARTC_SRC_ADI_EVENT_HIST,CENSUS_EE,BENEFICIARY,PARTICIPANT_LOG,PERSON_LOG,BENEFICIARY_LOG,PLAN_SOURCE,MEMBER_STATUS_HISTORY,ISW_SUSPENSION"
-export INCLUDE_TABLES="DATA_EXCHANGE_DEMO"
+export INCLUDE_TABLES="PERSON,EXTERNAL_ADDRESS,partc_status_cde,PLAN_STATUS_CDE,PRODUCT_TYPE_CDE,MARITAL_STATUS_CDE,PARTC_STATUS_HIST,PARTC_SOURCE,PLAN_DCO,PARTICIPANT,PLAN_SPONSOR,PLAN_SUBSCRIPTION,BUSINESS_ENTITY,PARTC_SRC_ADI_EVENT,PARTC_SRC_ADI_EVENT_HIST,CENSUS_EE,BENEFICIARY,PARTICIPANT_LOG,PERSON_LOG,BENEFICIARY_LOG,PLAN_SOURCE,MEMBER_STATUS_HISTORY,ISW_SUSPENSION,MEMBERS,DATA_EXCHANGE_HEADER,DATA_EXCHANGE_DEMO,PAYROLL_HISTORY,XPNS_INTERMITTENT_PIT,SPON_ACTIV_GRP,SPON_ACTIV_GRP_OPTION,PARTN1_PARTC_PERIODIC_BAL"
 export INCLUDE_VIEWS=1
 export SKIP_SOURCE_ROW_COUNT=1
 export CONNECTION_ID=reflex
-
-
 
 export JSON_INFO='
 {
@@ -20,22 +17,22 @@ export JSON_INFO='
               "upperBound": 9000000,
               "numPartitions": 50
             },
-            "TABLE1": {
-              "partitionColumn": "FIELD",
-              "lowerBound": 1,
-              "upperBound": 22000000,
+            "PAYROLL_HISTORY": {
+              "partitionColumn": "SUBSCRIBER_ID",
+              "lowerBound": 1000,
+              "upperBound": 216000,
               "numPartitions": 50
             },
-            "TABLE2": {
-              "partitionColumn": "FIELD",
-              "lowerBound": 5600000,
-              "upperBound": 34000000,
+            "PARTN1_PARTC_PERIODIC_BAL": {
+              "partitionColumn": "SUBSCRIBER_ID",
+              "lowerBound": 3,
+              "upperBound": 216000,
               "numPartitions": 50
             },
-            "TABLE3": {
-              "partitionColumn": "FIELD",
-              "lowerBound": 5700000,
-              "upperBound": 40000000,
+            "CENSUS_EE": {
+              "partitionColumn": "SUBSCRIBER_ID",
+              "lowerBound": 100,
+              "upperBound": 216000,
               "numPartitions": 50
             }
         }
