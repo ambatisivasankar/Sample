d4e9bad7fea12a400b58c63e854994d759dc92ee
Nicholas Chammas <nicholas.chammas@gmail.com>
2018-07-16 16:24:28 -0400

    [INGEST-367] Partition large DRC tables (#225)
    * partition large DRC tables
    * try 50 partitions

--- squark-classic/jobs/drc.sh
+++ squark-classic/jobs/drc.sh
@@ -4,6 +4,7 @@ export SQL_TEMPLATE='%s'
 export INCLUDE_VIEWS=1
 export CONNECTION_ID=drc
 
+export SPARK_EXECUTOR_MEMORY="4G"
 export SPARK_MAX_EXECUTORS=15
 
 export EXCLUDE_TABLES_ARRAY=(
@@ -56,3 +57,24 @@ export INCLUDE_TABLES_ARRAY=(
 
 export EXCLUDE_TABLES="$(IFS=, ; echo "${EXCLUDE_TABLES_ARRAY[*]}")"
 export INCLUDE_TABLES="$(IFS=, ; echo "${INCLUDE_TABLES_ARRAY[*]}")"
+
+export JSON_INFO="
+{
+    'PARTITION_INFO':{
+        'tables': {
+            'SLS_AND_DTRB_FACT_VW': {
+              'partitionColumn': 'SLS_DTRB_SID',
+              'lowerBound': 1,
+              'upperBound': 80000000,
+              'numPartitions': 50
+            },
+            'NFF_FACT_VW': {
+              'partitionColumn': 'NFF_SID',
+              'lowerBound': 1,
+              'upperBound': 10000000,
+              'numPartitions': 50
+            }
+        }
+    }
+}
+"
