ccfc25b8d254930e461bbd47f87e45e32efce08f
Owen Galvin <ogalvin@massmutual.com>
2018-08-13 09:38:19 -0400

    try the full SELECT syntax for SQL_SUBQUERY

--- squark-classic/jobs/haven_cdmproducers.sh
+++ squark-classic/jobs/haven_cdmproducers.sh
@@ -12,7 +12,7 @@ export JSON_INFO="
 	'SAVE_TABLE_SQL_SUBQUERY':{
       'schema': 'dbo',
       'table_queries': {
-      		'commissionable_event_info': '(SELECT \\\"id\\\" FROM commissionable_event_info) as subquery'
+            'commissionable_event_info': '(SELECT \\\"id\\\",\\\"externalId\\\",\\\"eventType\\\",\\\"transactionDate\\\",\\\"producerId\\\",\\\"invoiceId\\\",\\\"originalExternalId\\\",\\\"productCode\\\",\\\"policyFaceAmount\\\",\\\"policyTerm\\\",\\\"value\\\",\\\"insuredLastName\\\",\\\"policyIssuedState\\\",\\\"securedDate\\\",\\\"policyIssuedDate\\\",\\\"policyEndDate\\\",\\\"terminationDate\\\",\\\"billingCycleDate\\\",\\\"policyEffectiveDate\\\",CAST(\\\"flatExtraAmount\\\" AS VARCHAR(65000)) AS flatExtraAmount,CAST(\\\"waiverPremiumAmount\\\" AS VARCHAR(65000)) AS waiverPremiumAmount,\\\"createdAt\\\",\\\"updatedAt\\\",\\\"commissionableEventId\\\" FROM commissionable_event_info) as subquery'
         }
     }
 }"
