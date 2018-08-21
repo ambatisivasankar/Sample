d65a0868dde7f91c96032d2ed3cc7af4930bd741
Owen Galvin <ogalvin@massmutual.com>
2018-08-21 16:11:10 -0400

    filter the get-projection sql to only tables that exist

--- squark-classic/resources/get_projections_to_create.sql
+++ squark-classic/resources/get_projections_to_create.sql
@@ -1,7 +1,8 @@
-SELECT projection_sql
+SELECT s.projection_sql
 FROM admin.squark_vertica_projections s
--- after updating squark_vertica_projections to be per-table only, can INNER JOIN to filter on only tables that exist
---INNER JOIN all_tables a ON a.schema_name = s.schema_name AND a.table_name = s.table_names
+-- only add projections if related table exists in this schema
+INNER JOIN all_tables a ON a.schema_name = s.schema_name AND a.table_name = s.table_name
 WHERE 1=1
     AND s.is_enabled = 'True'
     AND s.schema_name = :VERTICA_SCHEMA
+ORDER BY s.id
