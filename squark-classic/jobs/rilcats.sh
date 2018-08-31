6f11158faa28c645a1ef9417f02a9810b985ccc4
Owen Galvin <ogalvin@massmutual.com>
2018-08-31 12:22:17 -0400

    [INGEST-425] cut down number of rilcats tables being ingested

--- squark-classic/jobs/rilcats.sh
+++ squark-classic/jobs/rilcats.sh
@@ -2,7 +2,9 @@ export PROJECT_ID=rilcats
 export WAREHOUSE_DIR='/_wh/'
 export SQL_TEMPLATE='%s'
 # Excluded due to discussion here: https://advana.atlassian.net/browse/INGEST-150
-export EXCLUDE_TABLES="wrk_txt_cmnt"
+# export EXCLUDE_TABLES="wrk_txt_cmnt"
+# 2018.08.31 adding an INCLUDE_TABLES to only those being accessed on Vertica, comment out above since it would be redundant
+export INCLUDE_TABLES="Acrcy_Adt,Anny_Wrk,Dept,div,Rqstr_Typ,Rsrc,stus,svc_chnl_cde,Wrk,wrk_cmnt,wrk_data,wrk_evnt,wrk_que,Wrk_Xtn"
 export CONNECTION_ID=rilcats
 # 2017.08.09, source row count queries adding < 20 minutes to reflex duration, worth it for now during testing & transition
 #export SKIP_SOURCE_ROW_COUNT=1
