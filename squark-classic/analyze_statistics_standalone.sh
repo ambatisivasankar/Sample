6b56680fce6139159ec659ce2f068f9f280b7af0
Owen Galvin <ogalvin@massmutual.com>
2018-09-13 09:39:30 -0400

    try using existing env-cluster infrastructure

--- squark-classic/analyze_statistics_standalone.sh
+++ squark-classic/analyze_statistics_standalone.sh
@@ -5,18 +5,19 @@
 analyze_statistics_sql="analyze_statistics.sql"
 
 stats_sql=$(cat <<-EOM
-SELECT 'SELECT ''' || t.table_schema || '.' || t.table_name || ''' as tbl,
-    ANALYZE_STATISTICS (''' || t.table_schema || '.' || t.table_name || ''');'
+SELECT 'SELECT ''' || t.table_schema || '.' || t.table_name || ''' as tbl, ANALYZE_STATISTICS (''' || t.table_schema || '.' || t.table_name || ''');'
 FROM tables t
 WHERE table_schema = '$STAT_SCHEMA_NAME';
 EOM
 )
 
 # 1) create the statements to execute, e.g.   SELECT ANALYZE_STATISTICS ('haven_daily.address');
-vsql -h $STAT_VERTICA_HOST -U $STAT_VERTICA_USER -w $STAT_VERTICA_PASSWORD -d advana -c "$stats_sql" -o $analyze_statistics_sql -At
+# vsql -h $STAT_VERTICA_HOST -U $STAT_VERTICA_USER -w $STAT_VERTICA_PASSWORD -d advana -c "$stats_sql" -o $analyze_statistics_sql -At
+vsql -h $VERTICA_HOST -U $VERTICA_USER -w $VERTICA_PASSWORD -d $VERTICA_DATABASE -c "$stats_sql" -o $analyze_statistics_sql -At
 
 cat $analyze_statistics_sql
 
 # 2) execute all of the ANALYZE_STATISTICS statements
-vsql -h $STAT_VERTICA_HOST -U $STAT_VERTICA_USER -w $STAT_VERTICA_PASSWORD -d advana -f $analyze_statistics_sql
+# vsql -h $STAT_VERTICA_HOST -U $STAT_VERTICA_USER -w $STAT_VERTICA_PASSWORD -d advana -f $analyze_statistics_sql
+vsql -h $VERTICA_HOST -U $VERTICA_USER -w $VERTICA_PASSWORD -d $VERTICA_DATABASE -f $analyze_statistics_sql
 
