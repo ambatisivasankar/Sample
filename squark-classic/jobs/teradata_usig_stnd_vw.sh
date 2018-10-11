b28d108e73a97789ebedecd20a2304cebc79e93a
Bryant Moscon <bmoscon@gmail.com>
2018-10-11 12:24:01 -0400

    INGEST-453

---
+++ squark-classic/jobs/teradata_usig_stnd_vw.sh
@@ -0,0 +1,10 @@
+export PROJECT_ID=teradata_usig_stnd_vw
+export WAREHOUSE_DIR='/_wh/'
+export SQL_TEMPLATE='%s'
+export INCLUDE_VIEWS=1
+export INCLUDE_TABLES="CUST_ADDL_TEL_NR_CMN_VW,CUST_ADDL_ELEC_AD_CMN_VW"
+export CONNECTION_ID=teradata_usig_stnd_vw
+
+
+
+export SPARK_MAX_EXECUTORS=10
