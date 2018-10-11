c5368aac2b76f694f25f121ff736417f0d6cff56
Owen Galvin <ogalvin@massmutual.com>
2018-10-11 15:43:40 -0400

    partition drug_indication, seems to be needing it

--- squark-classic/jobs/haven.sh
+++ squark-classic/jobs/haven.sh
@@ -32,6 +32,12 @@ export JSON_INFO="
               'upperBound': 59,
               'numPartitions': 59
             },
+            'drug_indication': {
+              'partitionColumn': 'DATE_PART('''SECOND''', COALESCE(\\\"createdTime\\\", '''1970-01-01T00:00:00'''::timestamp))',
+              'lowerBound': 0,
+              'upperBound': 59,
+              'numPartitions': 60
+            },
             'interaction': {
               'partitionColumn': 'DATE_PART('''MINUTE''', COALESCE(\\\"createdTime\\\", '''1970-01-01T00:00:00'''::timestamp))',
               'lowerBound': 0,
