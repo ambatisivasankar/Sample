8c1b3c7920a7b726e451ed3c274741c0c1a03c7d
Iryna Paluyanava-Pseftis <ipaluyanavapseftis@massmutual.com>
2018-12-11 18:29:15 -0500

    [DD-107] Added tables needed for b2b extracts

--- squark-classic/jobs/teradata_usig_stnd_vw.sh
+++ squark-classic/jobs/teradata_usig_stnd_vw.sh
@@ -2,9 +2,28 @@ export PROJECT_ID=teradata_usig_stnd_vw
 export WAREHOUSE_DIR='/_wh/'
 export SQL_TEMPLATE='%s'
 export INCLUDE_VIEWS=1
-export INCLUDE_TABLES="CUST_ADDL_TEL_NR_CMN_VW,CUST_ADDL_ELEC_AD_CMN_VW"
+export INCLUDE_TABLES="CUST_ADDL_TEL_NR_CMN_VW,CUST_ADDL_ELEC_AD_CMN_VW,PDCR_AGMT_CMN_VW,AGMT_CMN_VW,SLLNG_AGMT_CMN_VW,SALES_HIERARCHY_VW"
 export CONNECTION_ID=teradata_usig_stnd_vw
 
-
-
 export SPARK_MAX_EXECUTORS=10
+
+export JSON_INFO="
+{
+    'PARTITION_INFO':{
+        'tables': {
+            'AGMT_CMN_VW': {
+              'partitionColumn': 'AGREEMENT_ID',
+              'lowerBound': 1,
+              'upperBound': 34000000,
+              'numPartitions': 50
+            },
+            'PDCR_AGMT_CMN_VW': {
+              'partitionColumn': 'AGREEMENT_ID',
+              'lowerBound': 1,
+              'upperBound': 34000000,
+              'numPartitions': 50
+            },
+        }
+   }
+}
+"
