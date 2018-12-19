d4bf78745816818d66765fb31a2bae8d3aa54585
Owen Galvin <ogalvin@massmutual.com>
2018-12-19 11:10:05 -0500

    [DS-52] add partitioning for DBO_WOB_TIME_SERIES_VW

--- squark-classic/jobs/teradata_di_recovery.sh
+++ squark-classic/jobs/teradata_di_recovery.sh
@@ -4,3 +4,18 @@ export SQL_TEMPLATE='%s'
 export INCLUDE_VIEWS=1
 export INCLUDE_TABLES='DBO_DIBS_CLAIM_VW,DBO_WORK_OBJECT_VW,DBO_WOB_TIME_SERIES_VW,DBO_TREX_USERS_VW,DBO_TREX_WORKFLOW_VW,DBO_CONTENT_VW'
 export CONNECTION_ID=teradata_prod_dma
+
+
+export SPARK_MAX_EXECUTORS=10
+export JSON_INFO='{
+	"PARTITION_INFO": {
+		"tables": {
+			"DBO_WOB_TIME_SERIES_VW": {
+				"partitionColumn": "RWorkflowTimeSeriesID",
+				"lowerBound": 0,
+				"upperBound": 21000000,
+				"numPartitions": 50
+			}
+		}
+	}
+}'
