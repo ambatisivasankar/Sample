1ad3c216c555766969621da05d78d80f73f1f6cf
Owen Galvin <ogalvin@massmutual.com>
2019-01-04 14:37:10 -0500

    add partitioning to zd_ticket

--- squark-classic/jobs/haven_commhub.sh
+++ squark-classic/jobs/haven_commhub.sh
@@ -5,3 +5,17 @@ export CHECK_PRIVS=1
 export CONNECTION_ID=haven_commhub
 export SQUARK_METADATA=1
 
+export JSON_INFO="
+{
+    'PARTITION_INFO':{
+        'tables': {
+            'zd_ticket': {
+              'partitionColumn': 'id',
+              'lowerBound': 27000,
+              'upperBound': 600000,
+              'numPartitions': 50
+            }
+        }
+   }
+}
+"
