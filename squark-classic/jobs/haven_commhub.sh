1d06bd07a066ce46e58ba1c75cedb6e9be971ea0
Owen Galvin <ogalvin@massmutual.com>
2019-01-28 10:27:09 -0500

    add partitioning to haven_commhub.md_email_content_raw

--- squark-classic/jobs/haven_commhub.sh
+++ squark-classic/jobs/haven_commhub.sh
@@ -6,10 +6,17 @@ export CONNECTION_ID=haven_commhub
 export SQUARK_METADATA=1
 export CONVERT_ARRAYS_TO_STRING=1
 
+# md_email_content_raw, remove any letters (g=global, i=case-insensitive), return first two digits, which should be implicitly cast as INT for comparison purposes
 export JSON_INFO="
 {
     'PARTITION_INFO':{
         'tables': {
+            'md_email_content_raw': {
+              'partitionColumn': 'LEFT(regexp_replace(id, '''[a-z]''', '''''', '''gi'''), 2)',
+              'lowerBound': 0,
+              'upperBound': 99,
+              'numPartitions': 99
+            },
             'zd_ticket': {
               'partitionColumn': 'id',
               'lowerBound': 27000,
