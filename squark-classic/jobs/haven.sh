82b6b4876851e79955d7d7e2ed82ad0c18f32eda
Owen Galvin <ogalvin@massmutual.com>
2019-02-01 16:22:46 -0500

    haven, add standard SECOND partitioning to driving_records

--- squark-classic/jobs/haven.sh
+++ squark-classic/jobs/haven.sh
@@ -39,6 +39,12 @@ export JSON_INFO="
               'upperBound': 59,
               'numPartitions': 60
             },
+            'driving_records': {
+              'partitionColumn': 'DATE_PART('''SECOND''', COALESCE(\\\"createdTime\\\", '''1970-01-01T00:00:00'''::timestamp))',
+              'lowerBound': 0,
+              'upperBound': 59,
+              'numPartitions': 60
+            },
             'drug_indication': {
               'partitionColumn': 'DATE_PART('''SECOND''', COALESCE(\\\"createdTime\\\", '''1970-01-01T00:00:00'''::timestamp))',
               'lowerBound': 0,
