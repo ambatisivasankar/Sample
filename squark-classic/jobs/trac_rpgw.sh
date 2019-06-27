9f555d0c5e0cfdfbdf0a22160c34601ff97937d7
Andrew Sheridan <50179676+Andrew-Sheridan@users.noreply.github.com>
2019-06-27 12:30:37 -0400

    Merge dev into master (#332)
    * Changed Vertica host and port
    
    * haven incremental, update max-last-updated-query for Vertica 9.2
    
    v9.2 handling timestamps (w/o tz) diff vs. v8.1,
    remove "AT TIME ZONE 'GMT'" from query adjustment
    
    * haven migration - add v9 timezone fix, new haven eon RDS endpoint
    
    * Added fix for Copy command in AWS to use new format.
    
    * Updated passwords.
    
    * Fixed s3 copy path in dirload.
    
    * main haven job, point to haven_eon connection during eon migration
    
    * Change the way we generate virtual environment to avoid Jenkins package install issues
    
    * Update teradata_nb.sh
    
    Added 'NB_BLDED_SUBMIT_APPL_DTL_VW' to INCLUDE_TABLE
    
    * Update teradata_nb.sh
    
    Changed in teradata_nb.sh: Added 'NB_BLDED_SUBMIT_APPL_DTL_VW' to INCLUDE_TABLE.
    
    Note:
    The col and row of the "PROD_NBR_VW.NB_BLDED_SUBMIT_APPL_DTL_VW" is 53 x 874,507 (no partition needed)
    
    * updated source vs. Vertica row count reconciliation query for eon
    
    added constant "shard offset" that is working w/curr clusters
    note, new Vertica blog post has query for similar row count query
     using system tables but not easy to integrate with this SQL
     https://www.vertica.com/blog/table-row-counts-by-schema/
    
    * eon row count check, add ENABLE_WITH_CLAUSE_MATERIALIZATION query hint
    
    * Ds 216 rebase (#327)
    
    * parent ac98cf826101ae21b838e8982d89edb142967335
    author Andrew Sheridan <ASheridan25@massmutual.com> 1559069829 -0400
    committer Andrew Sheridan <ASheridan25@massmutual.com> 1561137599 -0400
    
    DS-216 - Created `haven_uw.sh` to hold job info for new haven_uw schema in Vertica
    
    DS-216 - Updated `haven_uw.sh` and added source to config file
    
    Added hard break at start of save_table(). Just testing if the jdbc connection is good
    
    Removed hard break
    
    Added prototype exclude_schema flag
    
    Made exclude_schema a list
    
    Corrected KeyError typo
    
    Added PyCharm .idea to gitignore
    
    Added haven_uw to large_ddl list
    
    Moved EXCLUDE_SCHEMA env-var to match current squark layout.
    
    Adding standard whitespace
    
    Adding documentation for EXCLUDE_SCHEMA
    
    * added haven_uw to sources
    
    * [DS-601] (#330)
    
    * Added tables to INCLUDE_TABLES. Moved job code from Jenkins exceute-shell to job file.
    
    * Flipped single and double quotes.
    
    * Set timezone to GMT
    
    * Added partitioning for DIM_PARTICIPANT_ACCOUNT
    
    * Increased the upperbound partition value of DIM_PARTICIPANT. Each partition was about 0.45 million records, but the last one was 40million. Now each should be about 0.7 million.
    
    * Repartitioned all the tables which had pre-existing partitioning, based on outputs from DBeaver.
    
    * Added partitioning for DIM_BENEFICIARY
    
    * DS-564 (#331)

--- squark-classic/jobs/trac_rpgw.sh
+++ squark-classic/jobs/trac_rpgw.sh
@@ -1,28 +1,81 @@
 export PROJECT_ID=trac_rpgw
 export WAREHOUSE_DIR='/_wh/'
 export SQL_TEMPLATE='%s'
-#export INCLUDE_TABLES="FACT_PLAN_SOURCE_SUMMARY,FACT_DAILY_INCOME,FACT_LOAN_SOURCE_SUMMARY,FACT_PLAN_SUMMARY,FACT_PARTICIPANT_FEE,FACT_PRTCPNT_BLNC_SMMRY,FACT_PLAN_FNNCL_TRNSCTN,DIM_PARTICIPANT,FACT_PLAN_ACCOUNT_ASSETS,FACT_PLAN_ACCOUNT_FEE,DIM_PLAN,FACT_PRTCPNT_LOAN_SMMRY,FACT_PRTCPNT_ASST_DTL,FACT_SHORT_TERM_TRADE,FACT_PRTCPNT_LOAN_TRNSCTN,FACT_PRTCPNT_ASST_SMMRY,FACT_PARTICIPANT_ACCOUNT"
-export INCLUDE_TABLES="DIM_PARTICIPANT"
 export SKIP_MIN_MAX_ON_CAST=1
 export CONNECTION_ID=trac_rpgw
-
 # connection attempt on aws jenkins-ingestion fails without below TZ setting: ORA-01882: timezone region  not found
+# 2018.06.24 TZ still needs to be set
 export TZ=GMT
-export SPARK_MAX_EXECUTORS=10
-export JSON_INFO="
+export INCLUDE_TABLES='DIM_AFFILIATE_COMPANY,DIM_ICU,DIM_PARTICIPANT_ACCOUNT,DIM_PLAN_ACCOUNT,DIM_PARTICIPANT,DIM_ACCOUNT_ADMNSTRTR,DIM_SPONSOR_COMPANY,DIM_VRU_ENROLLMENT_STATUS,DIM_CONSENT_CUST,DIM_BENEFICIARY,DIM_PLAN,DIM_EMPLOYEE,FACT_PRTCPNT_ASST_DTL,DIM_RETIREMENT_PROJECTION'
+export SPARK_MAX_EXECUTORS=20
+export JSON_INFO='
 {
-    'PARTITION_INFO':{
-        'tables': {
-            'DIM_PARTICIPANT': {
-              'partitionColumn': 'PRTCPNT_KEY',
-              'lowerBound': 1,
-              'upperBound': 200000000,
-              'numPartitions': 200
+    "PARTITION_INFO":{
+        "tables": {
+            "DIM_CONSENT_CUST": {
+              "partitionColumn": "CUSTOMER_ID2",
+              "lowerBound": 0,
+              "upperBound": 94294966676,
+              "numPartitions": 50
+            },
+            "DIM_BENEFICIARY": {
+              "partitionColumn": "BNFCRY_KEY",
+              "lowerBound": 1000000,
+              "upperBound": 3000000,
+              "numPartitions": 100
+            },
+            "DIM_VRU_ENROLLMENT_STATUS": {
+              "partitionColumn": "INTRNL_PLAN_ID",
+              "lowerBound": 0,
+              "upperBound": 50000,
+              "numPartitions": 50
+            },
+            "DIM_PARTICIPANT_ALLCTN_STP": {
+              "partitionColumn": "rcrd_trm_dt >= """01-JAN-2016""" AND PRTCPNT_ALLCTN_STP_KEY",
+              "lowerBound": 0,
+              "upperBound": 550000000,
+              "numPartitions": 50
+            },
+            "DIM_EMPLOYEE": {
+              "partitionColumn": "ACTV_RCRD_IND = """Y""" AND EMPLY_KEY",
+              "lowerBound": 29000000,
+              "upperBound": 90000000,
+              "numPartitions": 50
+            },
+            "DIM_PLAN": {
+              "partitionColumn": "ACTV_RCRD_IND = """Y""" AND PLAN_KEY",
+              "lowerBound": 14000000,
+              "upperBound": 15000000,
+              "numPartitions": 50
+            },
+            "FACT_PLAN_ACCOUNT_FEE": {
+              "partitionColumn": "PLAN_ACCNT_KEY",
+              "lowerBound": 700000,
+              "upperBound": 2100000,
+              "numPartitions": 50
+            },
+            "FACT_PRTCPNT_ASST_DTL": {
+              "partitionColumn": "ASST_AS_OF_DT >= """01-JAN-2016""" AND PLAN_FUND_SUB_ACCNT_KEY",
+              "lowerBound": 2500000,
+              "upperBound": 6500000,
+              "numPartitions": 100
+            },
+            "DIM_PARTICIPANT": {
+              "partitionColumn": "RCRD_TRM_DT >= """01-JAN-2016""" AND PRTCPNT_KEY",
+              "lowerBound": 168220000,
+              "upperBound": 240000000,
+              "numPartitions": 100
+            },
+            "DIM_PARTICIPANT_ACCOUNT": {
+              "partitionColumn": "PRTCPNT_ACCNT_KEY",
+              "lowerBound": 50000000,
+              "upperBound": 100000000,
+              "numPartitions": 100
             }
         }
    }
 }
-"
+'
 
 #export JSON_INFO="
 #{'SAVE_TABLE_SQL_SUBQUERY':{
