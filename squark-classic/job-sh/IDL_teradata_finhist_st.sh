49c074c352491356fdfb04834ea43e0b9d3910c7
Andrew Sheridan <ASheridan25@massmutual.com>
2020-01-15 14:08:31 -0500

    env now a var

--- squark-classic/job-sh/IDL_teradata_finhist_st.sh
+++ squark-classic/job-sh/IDL_teradata_finhist_st.sh
@@ -6,8 +6,8 @@ for _ in {1..121}
 
   export OTHER_VARIABLES="-e -v schema_name=squark_staging -At -o output_file "
 
-  ./load_tables.bash dev --sql-file=squark-classic/job-sql/${squark_name}/truncate_AGMT_FIN_TXN_CMN_VW.sql
-  ./load_tables.bash dev --sql-file=squark-classic/job-sql/${squark_name}/get_batch_dates_AGMT_FIN_TXN_CMN_VW.sql
+  ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/truncate_AGMT_FIN_TXN_CMN_VW.sql
+  ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/get_batch_dates_AGMT_FIN_TXN_CMN_VW.sql
 
   export strt_dt=`cat output_file | cut -d'|' -f2`
   export end_dt=`cat output_file | cut -d'|' -f3`
@@ -20,9 +20,9 @@ for _ in {1..121}
       exit 0
     else
       export OTHER_VARIABLES="-e -v schema_name=squark_staging -At -o output_file -v strt_dt='${strt_dt}' -v end_dt='${end_dt}'"
-      ./launch_squark_job.sh --dev --parquet --skip-schema ${squark_name}
-      ./load_tables.bash dev --sql-file=squark-classic/job-sql/${squark_name}/insert_batch_AGMT_FIN_TXN_CMN_VW.sql
-      ./load_tables.bash dev --sql-file=squark-classic/job-sql/${squark_name}/update_batch_dates_AGMT_FIN_TXN_CMN_VW.sql
+      ./launch_squark_job.sh --"${ENV}" --parquet --skip-schema ${squark_name}
+      ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/insert_batch_AGMT_FIN_TXN_CMN_VW.sql
+      ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/update_batch_dates_AGMT_FIN_TXN_CMN_VW.sql
   fi
 
 done
