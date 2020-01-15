70b0c31fc174a27ea907cb4a8234626639fd8961
Andrew Sheridan <ASheridan25@massmutual.com>
2020-01-15 13:29:03 -0500

    Moved IDL code into shell script

--- squark-classic/job-sh/IDL_teradata_finhist_st.sh
+++ squark-classic/job-sh/IDL_teradata_finhist_st.sh
@@ -1,32 +1,27 @@
 
-# This should go up to the number of records in the table squark_staging.WRK_LD_BTCH_AGMT_FIN_TXN_CMN_VW_st
-export IDL=1
-NUM_LOOPS=${NUM_LOOPS:-125}
-
-for ((i=0;i<=NUM_LOOPS; i++));
+# This should go up to the number of records in the table squark_staging.WRK_LD_BTCH_AGMT_FIN_TXN_CMN_VW
+for _ in {1..121}
   do
-    echo "Loop Number: ${i}"
-    export OTHER_VARIABLES="-e -v schema_name=squark_staging -At -o output_file "
 
-    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/count_staging_AGMT_FIN_TXN_CMN_VW_st.sql
-    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/truncate_staging_AGMT_FIN_TXN_CMN_VW_st.sql
-    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/get_batch_dates_AGMT_FIN_TXN_CMN_VW_st.sql
+  export OTHER_VARIABLES="-e -v schema_name=squark_staging -At -o output_file "
 
-    export strt_dt=`cat output_file | cut -d'|' -f2`
-    export end_dt=`cat output_file | cut -d'|' -f3`
+  ./load_tables.bash dev --sql-file=squark-classic/job-sql/${squark_name}/truncate_AGMT_FIN_TXN_CMN_VW.sql
+  ./load_tables.bash dev --sql-file=squark-classic/job-sql/${squark_name}/get_batch_dates_AGMT_FIN_TXN_CMN_VW.sql
 
-    echo "Start date=${strt_dt}"
-    echo "End date=${end_dt}"
+  export strt_dt=`cat output_file | cut -d'|' -f2`
+  export end_dt=`cat output_file | cut -d'|' -f3`
 
-  if [ -z "${strt_dt}" ]
+  echo "Start date=${strt_dt}"
+  echo "End date=${end_dt}"
+  if [ -z "${end_dt+x}" ]
     then
-      echo "No start date - skipping IDL"
-      exit 0
+      echo "No end date - skipping IDL"
+      break
     else
       export OTHER_VARIABLES="-e -v schema_name=squark_staging -At -o output_file -v strt_dt='${strt_dt}' -v end_dt='${end_dt}'"
-      ./launch_squark_job.sh --"${ENV}" --parquet --skip-schema ${squark_name}
-      ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/insert_IDL_batch_AGMT_FIN_TXN_CMN_VW_st.sql
-      ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/update_batch_dates_AGMT_FIN_TXN_CMN_VW_st.sql
+      ./launch_squark_job.sh --dev --parquet --skip-schema ${squark_name}
+      ./load_tables.bash dev --sql-file=squark-classic/job-sql/${squark_name}/insert_batch_AGMT_FIN_TXN_CMN_VW.sql
+      ./load_tables.bash dev --sql-file=squark-classic/job-sql/${squark_name}/update_batch_dates_AGMT_FIN_TXN_CMN_VW.sql
   fi
 
 done
