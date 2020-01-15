2612060d9fdcf11cf0c73eba0330473606f41d18
Andrew Sheridan <ASheridan25@massmutual.com>
2020-01-15 13:54:50 -0500

    Adjusting json info

--- squark-classic/jobs/teradata_finhist_st.sh
+++ squark-classic/jobs/teradata_finhist_st.sh
@@ -19,12 +19,6 @@ export INCLUDE_TABLES=$include_tables
 # IDL = Initial Date Load 0 = False, 1 = True (set in jenkins, defaults to False if not set)
 IDL=${IDL:-0}
 
-# DELTA_RANGE = Number of Dates to use for Incremetal/Delta query. (set in jenkins, defaults to 7 if not set)
-DELTA_RANGE=${DELTA_RANGE:-7}
-
-# PARTS = Number of partitions to use for Incremantal/Delta query (set in jenkins, defaults to 2 if not set)
-PARTS=${PARTS:-2}
-
 # Need single quotes for this json object because double quotes will not epand variables
 # $strt_dt and $end_dt are set in Jenkins at execution time
 if [ "${IDL}" -eq 1 ]; then
@@ -45,6 +39,11 @@ if [ "${IDL}" -eq 1 ]; then
 else
   echo "Setting JSON_INFO for incremental load"
 
+  # DELTA_RANGE = Number of Dates to use for Incremetal/Delta query. (set in jenkins, defaults to 7 if not set)
+  DELTA_RANGE=${DELTA_RANGE:-7}
+
+  # PARTS = Number of partitions to use for Incremantal/Delta query (set in jenkins, defaults to 2 if not set)
+  PARTS=${PARTS:-2}
   # If end date is not set then use current date as end date
   current_date=$(date '+%Y-%m-%d')
   export end_dt=${end_dt:-${current_date}}
@@ -54,10 +53,10 @@ else
       'SAVE_TABLE_SQL_SUBQUERY':{
           'AGMT_FIN_TXN_CMN_VW': {
               'sql_query': '(SELECT * FROM ST_A_USIG_STND_VW.AGMT_FIN_TXN_CMN_VW where TRANS_DT  between cast('''$end_dt''' as date) - '''$DELTA_RANGE''' AND cast('''$end_dt''' as date)) as subquery',
-              'numPartitions': '''$PARTS''',
-              'partitionColumn': '(AGREEMENT_ID Mod '''$PARTS''')',
+              'numPartitions': $PARTS,
+              'partitionColumn': '(AGREEMENT_ID Mod $PARTS)',
               'lowerBound': 0,
-              'upperBound': '''$PARTS'''
+              'upperBound': $PARTS
           }
       }
   }
