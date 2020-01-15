97a1b7d29e5545bc357294db58f063090a8288af
Andrew Sheridan <ASheridan25@massmutual.com>
2020-01-15 14:16:47 -0500

    Added incremental batch inserting

---
+++ squark-classic/job-sql/teradata_finhist_st/insert_incremental_batch_AGMT_FIN_TXN_CMN_VW.sql
@@ -0,0 +1,22 @@
+--- delete data for this date batch from the target table
+--- TRANS_DT is the date column used for INCREMENTAL batches
+DELETE FROM
+    teradata_finhist_st.AGMT_FIN_TXN_CMN_VW
+WHERE
+    TRANS_DT IN (
+        SELECT
+            TRANS_DT
+        FROM
+            squark_staging.AGMT_FIN_TXN_CMN_VW
+        GROUP BY 1
+    )
+;
+
+--- Insert all the data from staging into target
+INSERT INTO teradata_finhist_st.AGMT_FIN_TXN_CMN_VW
+SELECT
+    *
+FROM
+    squark_staging.AGMT_FIN_TXN_CMN_VW
+;
+COMMIT;
