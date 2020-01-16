008886e23334f496ed78286e446a35d3e071fff6
Andrew Sheridan <ASheridan25@massmutual.com>
2020-01-16 10:17:48 -0500

    Added some counts for debugging

--- squark-classic/job-sql/teradata_finhist_st/insert_incremental_batch_AGMT_FIN_TXN_CMN_VW.sql
+++ squark-classic/job-sql/teradata_finhist_st/insert_incremental_batch_AGMT_FIN_TXN_CMN_VW.sql
@@ -1,3 +1,10 @@
+--- Count the records, used for debugging
+SELECT
+    COUNT(1)
+FROM
+    squark_staging.AGMT_FIN_TXN_CMN_VW
+;
+
 --- delete data for this date batch from the target table
 --- TRANS_DT is the date column used for INCREMENTAL batches
 DELETE FROM
--- squark-classic/job-sql/teradata_finhist_st/truncate_AGMT_FIN_TXN_CMN_VW.sql
+++ squark-classic/job-sql/teradata_finhist_st/truncate_AGMT_FIN_TXN_CMN_VW.sql
@@ -1,1 +1,9 @@
+--- Count the records, used for debugging
+SELECT
+    COUNT(1)
+FROM
+    squark_staging.AGMT_FIN_TXN_CMN_VW
+;
+
+
 TRUNCATE TABLE squark_staging.AGMT_FIN_TXN_CMN_VW;
