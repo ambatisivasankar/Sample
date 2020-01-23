bcaba130ad15c21bb1f06b3ef56754255b035cd6
Andrew Sheridan <ASheridan25@massmutual.com>
2020-01-23 10:09:41 -0500

    Added OPTIONAL IDL truncation

--- squark-classic/job-sh/IDL_teradata_finhist_qa.sh
+++ squark-classic/job-sh/IDL_teradata_finhist_qa.sh
@@ -2,24 +2,31 @@
 # This should go up to the number of records in the table squark_staging.WRK_LD_BTCH_AGMT_FIN_TXN_CMN_VW_qa
 export IDL=1
 NUM_LOOPS=${NUM_LOOPS:-125}
+TRUNCATE=${TRUNCATE:-0}
+if ((TRUNCATE == 1))
+  then
+    echo "WILL TRUNCATE ACTUAL TABLE BEFORE IDL"
+    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/count_actual_AGMT_FIN_TXN_CMN_VW.sql
+    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/truncate_actual_AGMT_FIN_TXN_CMN_VW.sql
+  else
+    echo "NOT TRUNCATING ACTUAL TABLE"
+fi
 
-./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/count_actual_AGMT_FIN_TXN_CMN_VW.sql
-./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/truncate_actual_AGMT_FIN_TXN_CMN_VW.sql
-
-for _ in {1..121}
+for ((i=0;i<=NUM_LOOPS; i++));
   do
+    echo "Loop Number: ${i}"
+    export OTHER_VARIABLES="-e -v schema_name=squark_staging -At -o output_file "
 
-  export OTHER_VARIABLES="-e -v schema_name=squark_staging -At -o output_file "
+    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/count_staging_AGMT_FIN_TXN_CMN_VW_qa.sql
+    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/truncate_staging_AGMT_FIN_TXN_CMN_VW_qa.sql
+    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/get_batch_dates_AGMT_FIN_TXN_CMN_VW_qa.sql
 
-  ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/count_staging_AGMT_FIN_TXN_CMN_VW_qa.sql
-  ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/truncate_staging_AGMT_FIN_TXN_CMN_VW_qa.sql
-  ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/get_batch_dates_AGMT_FIN_TXN_CMN_VW_qa.sql
+    export strt_dt=`cat output_file | cut -d'|' -f2`
+    export end_dt=`cat output_file | cut -d'|' -f3`
 
-  export strt_dt=`cat output_file | cut -d'|' -f2`
-  export end_dt=`cat output_file | cut -d'|' -f3`
+    echo "Start date=${strt_dt}"
+    echo "End date=${end_dt}"
 
-  echo "Start date=${strt_dt}"
-  echo "End date=${end_dt}"
   if [ -z "${strt_dt}" ]
     then
       echo "No start date - skipping IDL"
@@ -29,7 +36,6 @@ for _ in {1..121}
       ./launch_squark_job.sh --"${ENV}" --parquet --skip-schema ${squark_name}
       ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/insert_IDL_batch_AGMT_FIN_TXN_CMN_VW_qa.sql
       ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/update_batch_dates_AGMT_FIN_TXN_CMN_VW_qa.sql
-
   fi
 
 done
--- squark-classic/job-sh/IDL_teradata_finhist_st.sh
+++ squark-classic/job-sh/IDL_teradata_finhist_st.sh
@@ -2,23 +2,30 @@
 # This should go up to the number of records in the table squark_staging.WRK_LD_BTCH_AGMT_FIN_TXN_CMN_VW_st
 export IDL=1
 NUM_LOOPS=${NUM_LOOPS:-125}
-
-./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/count_actual_AGMT_FIN_TXN_CMN_VW.sql
-./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/truncate_actual_AGMT_FIN_TXN_CMN_VW.sql
+TRUNCATE=${TRUNCATE:-0}
+if ((TRUNCATE == 1))
+  then
+    echo "WILL TRUNCATE ACTUAL TABLE BEFORE IDL"
+    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/count_actual_AGMT_FIN_TXN_CMN_VW.sql
+    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/truncate_actual_AGMT_FIN_TXN_CMN_VW.sql
+  else
+    echo "NOT TRUNCATING ACTUAL TABLE"
+fi
 
 for ((i=0;i<=NUM_LOOPS; i++));
   do
-  echo "Loop Number: ${i}"
-  export OTHER_VARIABLES="-e -v schema_name=squark_staging -At -o output_file "
-  ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/count_staging_AGMT_FIN_TXN_CMN_VW_st.sql
-  ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/truncate_staging_AGMT_FIN_TXN_CMN_VW_st.sql
-  ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/get_batch_dates_AGMT_FIN_TXN_CMN_VW_st.sql
+    echo "Loop Number: ${i}"
+    export OTHER_VARIABLES="-e -v schema_name=squark_staging -At -o output_file "
 
-  export strt_dt=`cat output_file | cut -d'|' -f2`
-  export end_dt=`cat output_file | cut -d'|' -f3`
+    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/count_staging_AGMT_FIN_TXN_CMN_VW_st.sql
+    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/truncate_staging_AGMT_FIN_TXN_CMN_VW_st.sql
+    ./load_tables.bash "${ENV}" --sql-file=squark-classic/job-sql/${squark_name}/get_batch_dates_AGMT_FIN_TXN_CMN_VW_st.sql
 
-  echo "Start date=${strt_dt}"
-  echo "End date=${end_dt}"
+    export strt_dt=`cat output_file | cut -d'|' -f2`
+    export end_dt=`cat output_file | cut -d'|' -f3`
+
+    echo "Start date=${strt_dt}"
+    echo "End date=${end_dt}"
 
   if [ -z "${strt_dt}" ]
     then
