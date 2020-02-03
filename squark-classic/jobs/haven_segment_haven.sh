2f4971c65a555c701ef12a20686d3001aa7d2d74
Andrew Sheridan <ASheridan25@massmutual.com>
2020-02-03 18:23:25 -0500

    Added partitioning for 'pages'

--- squark-classic/jobs/haven_segment_haven.sh
+++ squark-classic/jobs/haven_segment_haven.sh
@@ -3,8 +3,7 @@ export WAREHOUSE_DIR='/_wh/'
 export SQL_TEMPLATE='"%s"'
 export CHECK_PRIVS=1
 export CONNECTION_ID=haven_segment_haven
-#export EXCLUDE_SCHEMA="haven_dtc_blog_production,haven_dtc_blog_backend_production,haven_dtc_backend_stage"
-export SPARK_MAX_EXECUTORS=60
+export SPARK_MAX_EXECUTORS=30
 
 export JSON_INFO="
 {
@@ -15,7 +14,13 @@ export JSON_INFO="
               'lowerBound': 0,
               'upperBound': 59,
               'numPartitions': 59
-            }
+            },
+            'pages': {
+              'partitionColumn': 'DATE_PART('''MINUTE''', COALESCE(\\\"timestamp\\\", '''1970-01-01T00:00:00'''::timestamp))',
+              'lowerBound': 0,
+              'upperBound': 59,
+              'numPartitions': 59
+            },
         }
    }
 }
