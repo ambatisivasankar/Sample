e6c6f6599d8e97dbf94a1d53b0706c8f9109f225
JCuiMM <jcui32@massmutual.com>
2020-02-03 16:34:02 -0500

    testing application_question_answered

--- squark-classic/jobs/haven_segment_haven.sh
+++ squark-classic/jobs/haven_segment_haven.sh
@@ -4,3 +4,19 @@ export SQL_TEMPLATE='"%s"'
 export CHECK_PRIVS=1
 export CONNECTION_ID=haven_segment_haven
 #export EXCLUDE_SCHEMA="haven_dtc_blog_production,haven_dtc_blog_backend_production,haven_dtc_backend_stage"
+export SPARK_MAX_EXECUTORS=60
+
+export JSON_INFO="
+{
+    'PARTITION_INFO':{
+        'tables': {
+            'application_question_answered': {
+              'partitionColumn': 'DATE_PART('''MINUTE''', COALESCE(\\\"timestamp\\\", '''1970-01-01T00:00:00'''::timestamp))',
+              'lowerBound': 0,
+              'upperBound': 59,
+              'numPartitions': 59
+            }
+        }
+   }
+}
+"
