15c2d781604fd21eef939c4e5548f317f3095beb
gkapoor58 <gkapoor58@massmutual.com>
2020-02-20 14:40:38 -0500

    Modified prod_stnd_prty job

--- squark-classic/jobs/prod_stnd_prty1_bkp.sh
+++ squark-classic/jobs/prod_stnd_prty1_bkp.sh
@@ -3,8 +3,8 @@ export PROJECT_ID=prod_stnd_prty1
 export WAREHOUSE_DIR='/_wh/'
 export SQL_TEMPLATE='%s'
 export INCLUDE_VIEWS=1
-export INCLUDE_TABLES='PDCR_DEMOGRAPHICS_PHV'
-export CONNECTION_ID=teradata_prod_stnd_prty
+export INCLUDE_TABLES='PDCR_DEMOGRAPHICS_VW'
+export CONNECTION_ID=teradata
 
 export SPARK_MAX_EXECUTORS=10
 
@@ -12,7 +12,7 @@ export JSON_INFO="
 {
     'PARTITION_INFO':{
         'tables': {
-            'PDCR_DEMOGRAPHICS_PHV': {
+            'PDCR_DEMOGRAPHICS_VW': {
               'partitionColumn': 'PRTY_ID mod 10',
               'lowerBound': 0,
               'upperBound': 9,
