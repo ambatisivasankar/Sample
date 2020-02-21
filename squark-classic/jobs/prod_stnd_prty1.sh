85c53164b665ece1db9f9bff0383102a7d64206a
gkapoor58 <gkapoor58@massmutual.com>
2020-02-20 20:37:06 -0500

    Job file prod stnd prty modified

--- squark-classic/jobs/prod_stnd_prty1.sh
+++ squark-classic/jobs/prod_stnd_prty1.sh
@@ -1,7 +1,7 @@
 export PROJECT_ID=prod_stnd_prty1
 # primary purpose of schema is to refresh select schemas on a daily basis
-export WAREHOUSE_DIR='/_wh/'
-export SQL_TEMPLATE='%s'
+export WAREHOUSE_DIR="/_wh/"
+export SQL_TEMPLATE="%s"
 export INCLUDE_VIEWS=1
 export CONNECTION_ID=teradata_prod_stnd_prty
 
@@ -26,85 +26,85 @@ export SPARK_MAX_EXECUTORS=10
 
 export JSON_INFO="
 {
-    'PARTITION_INFO':{
-        'tables': {
-            'PDCR_DEMOGRAPHICS_PHV': {
-              'partitionColumn': 'PRTY_ID mod 10',
-              'lowerBound': 0,
-              'upperBound': 9,
-              'numPartitions': 10
+    "PARTITION_INFO":{
+        "tables": {
+            "PDCR_DEMOGRAPHICS_PHV": {
+              "partitionColumn": "PRTY_ID mod 10",
+              "lowerBound": 0,
+              "upperBound": 9,
+              "numPartitions": 10
             },
-             'CUST_AGMT_CMN_PHV': {
-              'partitionColumn': 'oreplace((prty_id || agreement_id),' ','') mod 10',
-              'lowerBound': 0,
-              'upperBound': 9,
-              'numPartitions': 10
+             "CUST_AGMT_CMN_PHV": {
+              "partitionColumn": "oreplace((prty_id || agreement_id),' ','') mod 10",
+              "lowerBound": 0,
+              "upperBound": 9,
+              "numPartitions": 10
             },
-             'CUST_DEMOGRAPHICS_PHV': {
-              'partitionColumn': 'PRTY_ID MOD 50',
-              'lowerBound': 0,
-              'upperBound': 50,
-              'numPartitions': 50
+             "CUST_DEMOGRAPHICS_PHV": {
+              "partitionColumn": "PRTY_ID MOD 50",
+              "lowerBound": 0,
+              "upperBound": 50,
+              "numPartitions": 50
             },
-            'PRTY': {
-              'partitionColumn': 'PRTY_ID MOD 50',
-              'lowerBound': 0,
-              'upperBound': 50,
-              'numPartitions': 50
+            "PRTY": {
+              "partitionColumn": "PRTY_ID MOD 50",
+              "lowerBound": 0,
+              "upperBound": 50,
+              "numPartitions": 50
             },
-            'PRTY_AD': {
-              'partitionColumn': 'PRTY_ID MOD 50',
-              'lowerBound': 0,
-              'upperBound': 50,
-              'numPartitions': 50
+            "PRTY_AD": {
+              "partitionColumn": "PRTY_ID MOD 50",
+              "lowerBound": 0,
+              "upperBound": 50,
+              "numPartitions": 50
             },
-            'PRTY_ALT_ID': {
-              'partitionColumn': 'PRTY_ID MOD 50',
-              'lowerBound': 0,
-              'upperBound': 50,
-              'numPartitions': 50
+            "PRTY_ALT_ID": {
+              "partitionColumn": "PRTY_ID MOD 50",
+              "lowerBound": 0,
+              "upperBound": 50,
+              "numPartitions": 50
             },
-            'PRTY_SLLNG_AGMT': {
-              'partitionColumn': 'CONTR_ID MOD 50',
-              'lowerBound': 0,
-              'upperBound': 50,
-              'numPartitions': 50
+            "PRTY_SLLNG_AGMT": {
+              "partitionColumn": "CONTR_ID MOD 50",
+              "lowerBound": 0,
+              "upperBound": 50,
+              "numPartitions": 50
             },
-            'SLLNG_AGMT': {
-              'partitionColumn': 'CONTR_ID MOD 50',
-              'lowerBound': 0,
-              'upperBound': 10,
-              'numPartitions': 10
+            "SLLNG_AGMT": {
+              "partitionColumn": "CONTR_ID MOD 50",
+              "lowerBound": 0,
+              "upperBound": 10,
+              "numPartitions": 10
             },
-            'SLLNG_AGMT_CMN_PHV': {
-              'partitionColumn': 'CONTR_ID MOD 10',
-              'lowerBound': 0,
-              'upperBound': 10,
-              'numPartitions': 10
+            "SLLNG_AGMT_CMN_PHV": {
+              "partitionColumn": "CONTR_ID MOD 10",
+              "lowerBound": 0,
+              "upperBound": 10,
+              "numPartitions": 10
             },
-            'SLLNG_AGMT_DTL': {
-              'partitionColumn': 'CONTR_ID MOD 50',
-              'lowerBound': 0,
-              'upperBound': 50,
-              'numPartitions': 50
+            "SLLNG_AGMT_DTL": {
+              "partitionColumn": "CONTR_ID MOD 50",
+              "lowerBound": 0,
+              "upperBound": 50,
+              "numPartitions": 50
             },
-            'SLLNG_AGMT_SM_SPLITS': {
-              'partitionColumn': 'CONTR_ID MOD 10',
-              'lowerBound': 0,
-              'upperBound': 10,
-              'numPartitions': 10 
+            "SLLNG_AGMT_SM_SPLITS": {
+              "partitionColumn": "CONTR_ID MOD 10",
+              "lowerBound": 0,
+              "upperBound": 10,
+              "numPartitions": 10 
            },
-           'DTCHD_OFC': {
-              'partitionColumn': 'AGY_PRTY_ID mod 10',
-              'lowerBound': 0,
-              'upperBound': 9,
-              'numPartitions': 10
+           "DTCHD_OFC": {
+              "partitionColumn": "AGY_PRTY_ID mod 10",
+              "lowerBound": 0,
+              "upperBound": 9,
+              "numPartitions": 10
             },
-           'BP_DNP_HIST': {
-              'partitionColumn': 'BP_DNP_HIST_SID MOD 10',
-              'lowerBound': 0,
-              'upperBound': 10,
-              'numPartitions': 10
+           "BP_DNP_HIST": {
+              "partitionColumn": "BP_DNP_HIST_SID MOD 10",
+              "lowerBound": 0,
+              "upperBound": 10,
+              "numPartitions": 10
             }
        }
    }
