2bd7669ec34ca552c77251bc8210c0a1f1e7ee6f
ParthDhir12 <pdhir44@massmutual.com>
2020-03-03 15:17:45 -0500

    creating the projection and edit the dim_participant column which was not in vertica

--- squark-classic/jobs/trac_rpgw.sh
+++ squark-classic/jobs/trac_rpgw.sh
@@ -29,7 +29,43 @@ INCLUDE_TABLES_ARRAY=(
 export INCLUDE_TABLES="$(IFS=, ; echo "${INCLUDE_TABLES_ARRAY[*]}")"
 
 export JSON_INFO='
-{
+{ "SUPER_PROJECTION_SETTINGS":
+    {
+        "tables":
+        {
+            "DIM_PARTICIPANT":
+            {
+                "projection_name": "DIM_PARTICIPANT_SQUARK",
+                "order_by_columns": "ACTV_RCRD_IND,ICU_ID,AFFLT_CMPNY_ID,INTRNL_PLAN_ID,SPNSR_CMPNY_ID,PLAN_ID,EMPLY_TAX_ID,PRTCPNT_ID,PRTCPNT_KEY",
+                "segment_by_columns": "PRTCPNT_ID"
+            },
+            "DIM_VRU_ENROLLMENT_STATUS":
+            {
+                "projection_name": "DIM_VRU_ENROLLMENT_STATUS_SQUARK",
+                "order_by_columns": "ACTV_RCRD_IND,ICU_ID,ENRLLMNT_STTS_CD,ENRLLMNT_STTS_RPRT_DT,PLAN_ENRLLMNT_DT,INTRNL_PLAN_ID,PLAN_ID,PRTCPNT_ID,VRU_ENRLLMNT_STTS_KEY",
+                "segment_by_columns": "PRTCPNT_ID"
+            },
+            "DIM_PLAN":
+            {
+                "projection_name": "DIM_PLAN_SQUARK",
+                "order_by_columns": "ACTV_RCRD_IND,ICU_ID,PLAN_STTS_DSCRPTN,PLAN_TYPE_CD,PLAN_ID,PLAN_KEY",
+                "segment_by_columns": ""
+            },
+            "DIM_EMPLOYEE":
+            {
+                "projection_name": "DIM_EMPLOYEE_SQUARK",
+                "order_by_columns": "ACTV_RCRD_IND,ICU_ID,AFFLT_CMPNY_ID,SPNSR_CMPNY_ID,EMPLY_TAX_ID,EMPLY_KEY",
+                "segment_by_columns": "EMPLY_TAX_ID"
+            },
+            "DIM_SPONSOR_COMPANY":
+            {
+                "projection_name": "DIM_SPONSOR_COMPANY_SQUARK",
+                "order_by_columns": "ICU_ID,SPNSR_CMPNY_TYPE_CD,SPNSR_CMPNY_ID,SPNSR_CMPNY_TAX_ID,SPNSR_CMPNY_NM,SPNSR_CMPNY_KEY",
+                "segment_by_columns": ""
+            }
+
+        }
+    },
     "PARTITION_INFO":{
         "tables": {
             "DIM_BENEFICIARY": {
