3033cd558aa501b37a40c60f81027d9134c4544e
Andrew Sheridan <ASheridan25@massmutual.com>
2020-03-23 09:32:30 -0400

    Reverted teradata_access.sh to previous state

--- squark-classic/jobs/teradata_access.sh
+++ squark-classic/jobs/teradata_access.sh
@@ -2,29 +2,5 @@ export PROJECT_ID=teradata_access
 export WAREHOUSE_DIR='/_wh/'
 export SQL_TEMPLATE='%s'
 export INCLUDE_VIEWS=1
+export INCLUDE_TABLES='CUST_DMGR_DATA_VW'
 export CONNECTION_ID=teradata_access
-export SPARK_MAX_EXECUTORS=5
-
-include_tables_array=(
-  "AFT_WHLSLR_HIER_VW"
-  "SALES_HIERARCHY_VW"
-)
-
-include_tables="$(IFS=, ; echo "${include_tables_array[*]}")"
-export INCLUDE_TABLES=$include_tables
-
-
-export JSON_INFO="
-{
-    'PARTITION_INFO':{
-        'tables': {
-            'SALES_HIERARCHY_VW': {
-              'partitionColumn': 'AGY_PRTY_ID MOD 5',
-              'lowerBound': 0,
-              'upperBound': 5,
-              'numPartitions': 5
-            }
-        }
-   }
-}
-"
