8028f5c097de98455580166964b977a410949894
Andrew Sheridan <ASheridan25@massmutual.com>
2020-03-30 18:16:56 -0400

    typo

--- squark-classic/jobs/teradata_siera_subset.sh
+++ squark-classic/jobs/teradata_siera_subset.sh
@@ -6,7 +6,7 @@ export SQL_TEMPLATE='%s'
 
 # Optional
 export INCLUDE_VIEWS=1
-export SKIP_SOURCE_ROW_COUNT=0
+export SKIP_SOURCE_ROW_COUNT=1
 export SPARK_MAX_EXECUTORS=6
 
 include_tables_array=(
@@ -22,7 +22,7 @@ export JSON_INFO="
     'SAVE_TABLE_SQL_SUBQUERY':{
         'SLS_RPTG_PREM_HIST_VW': {
             'sql_query': '(SELECT * FROM PROD_USIG_CRCOG_DM_RPTG_VW.SLS_RPTG_PREM_HIST_VW) as subquery',
-            'numPartitions': 10,
+            'numPartitions': 13,
             'partitionColummn': 'COALESCE(EXTRACT(MONTH FROM CYCLE_DT), 13)',
             'lowerBound': 0,
             'upperBound': 13
