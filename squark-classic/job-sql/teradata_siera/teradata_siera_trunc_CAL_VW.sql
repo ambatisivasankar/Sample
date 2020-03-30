354315abe4e018195b1faabd62eac90b036db93c
Andrew Sheridan <ASheridan25@massmutual.com>
2020-03-30 17:57:44 -0400

    Added files for teradata_siera_subset job

---
+++ squark-classic/job-sql/teradata_siera/teradata_siera_trunc_CAL_VW.sql
@@ -0,0 +1,4 @@
+
+
+ TRUNCATE TABLE teradata_siera.cal_vw
+;
---
+++ squark-classic/jobs/teradata_siera_subset.sh
@@ -0,0 +1,38 @@
+# Required
+export PROJECT_ID=teradata_siera
+export CONNECTION_ID=prod_usig_crcog_dm_rptg_vw
+export WAREHOUSE_DIR='/_wh/'
+export SQL_TEMPLATE='%s'
+
+# Optional
+export INCLUDE_VIEWS=1
+export SKIP_SOURCE_ROW_COUNT=0
+export SPARK_MAX_EXECUTORS=6
+# export SPARK_EXECUTOR_MEMORY="4G"
+echo "Start dt: " $strt_dt
+echo "End Dt: " $end_dt
+
+include_tables_array=(
+  "SLS_RPTG_PREM_HIST_VW"
+  "CAL_VW"
+)
+
+include_tables="$(IFS=, ; echo "${include_tables_array[*]}")"
+export INCLUDE_TABLES=$include_tables
+
+export JSON_INFO="
+{
+    'SAVE_TABLE_SQL_SUBQUERY':{
+        'SLS_RPTG_PREM_HIST_VW': {
+            'sql_query': '(SELECT * FROM PROD_USIG_CRCOG_DM_RPTG_VW.SLS_RPTG_PREM_HIST_VW) as subquery',
+            'numPartitions': 10,
+            'partitionColummn': 'COALESCE(EXTRACT(MONTH FROM CYCLE_DT), 13)',
+            'lowerBound': 0,
+            'upperBound': 13
+        },
+        'CAL_VW': {
+            'sql_query': '(SELECT * FROM PROD_USIG_CRCOG_DM_RPTG_VW.CAL_VW) as subquery',
+        }
+    }
+}
+"
