5c03ac1fe9ba43411ecf867a8d177dd8667b11be
Andrew Sheridan <50179676+Andrew-Sheridan@users.noreply.github.com>
2020-04-06 13:41:04 -0400

    Bugfix/dd 334/teradata siera (#461)
    * Fixed: SQL reflects the "select *" nature of the export
    
    * typo
    
    * Removing bad syntax

--- squark-classic/job-sql/teradata_siera/CAL_VW_MRG.sql
+++ squark-classic/job-sql/teradata_siera/CAL_VW_MRG.sql
@@ -1,8 +1,7 @@
---- remove the records in staging that are already present in the actual table
-DELETE FROM SQUARK_STAGING.teradata_siera_cal_vw WHERE (_advana_md5) in (select _advana_md5 from TERADATA_SIERA.CAL_VW);
-COMMIT;
+--- clear out TERADATA_SIERA.CAL_VW
+TRUNCATE TABLE TERADATA_SIERA.CAL_VW;
 
---- insert the remaining records
+--- insert the replacement records
 INSERT INTO TERADATA_SIERA.CAL_VW
 SELECT * FROM SQUARK_STAGING.teradata_siera_cal_vw ;
 COMMIT;
--- squark-classic/job-sql/teradata_siera/SLS_RPTG_PREM_HIST_VW_MRG.sql
+++ squark-classic/job-sql/teradata_siera/SLS_RPTG_PREM_HIST_VW_MRG.sql
@@ -1,8 +1,7 @@
---- remove the records in staging that are already present in the actual table
-DELETE FROM SQUARK_STAGING.teradata_siera_sls_rptg_prem_hist_vw WHERE (_advana_md5) in (select _advana_md5 from TERADATA_SIERA.SLS_RPTG_PREM_HIST_VW);
-COMMIT;
+--- clear out TERADATA_SIERA.SLS_RPTG_PREM_HIST_VW
+TRUNCATE TABLE TERADATA_SIERA.SLS_RPTG_PREM_HIST_VW;
 
---- insert the remaining records
+--- insert the replacement records
 INSERT INTO TERADATA_SIERA.SLS_RPTG_PREM_HIST_VW
 SELECT * FROM SQUARK_STAGING.teradata_siera_sls_rptg_prem_hist_vw ;
 COMMIT;
